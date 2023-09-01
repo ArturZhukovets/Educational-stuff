320a33d2a3fd7250df12c00b9e415bc311db0bee
gvensye <bnlvdn7483@gmail.com>
2023-09-01 16:21:14 +0300

    theory

---
+++ fast_api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from schemas import Book
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"key": "Hello"}
+
+@app.get('/{pk}')
+def get_item(pk: int, sex: str, age: float = None):
+    return {"key:": pk, "sex": sex, "age": age}
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
---
+++ fast_api/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date
+
+class Genre(BaseModel):
+    name: str
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summary: str
+    genres: list[Genre]
+    pages: int
